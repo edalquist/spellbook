aefc8a6e00652667406a3b344dab6ec9242cbe17
Eric Dalquist <eric.dalquist@gmail.com>
2014-03-07 23:07:24 -0800

    Save all state

--- js/controllers.js
+++ js/controllers.js
@@ -17,12 +17,23 @@ spellBookApp.controller('SpellBookCtrl', function ($scope, $location, $http) {
 	};
 
 	$scope.saveState = function() {
+		// Clears all parameters first
+		$location.url($location.path());
+
 		$location.search('cid', $scope.selectedClass.id);
+		if ($scope.selectedClass.specs) {
+			$location.search('sid', $scope.selectedSpec.id);
+		}
+		$location.search('l', $scope.selectedLevel);
     };
 
     $scope.loadState = function() {
     	var savedState = $location.search();
 	    $scope.selectedClass = $scope.findEntity(savedState.cid, $scope.classes);
+	    if ($scope.selectedClass.specs) {
+	    	$scope.selectedSpec = $scope.findEntity(savedState.sid, $scope.selectedClass.specs);
+	    }
+	    $scope.selectedLevel = savedState.l;
     };
 
     $scope.$on('$locationChangeSuccess', $scope.loadState);
