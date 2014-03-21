57b64c732015f99a28e6e2518d583ad7e130a991
Eric Dalquist <eric.dalquist@gmail.com>
2014-03-21 11:55:08 -0700

    Add bootstrap

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var spellBookApp = angular.module('spellBookApp', []); 
+var spellBookApp = angular.module('spellBookApp', []);
 spellBookApp.config(function($locationProvider) {
   $locationProvider.html5Mode(true);
 });
