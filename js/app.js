7383bb2d2bb3a75789349109fce9ebdca64e9b2f
Eric Dalquist <eric.dalquist@gmail.com>
2014-03-21 17:01:04 -0700

    CHECKPOINT

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var spellBookApp = angular.module('spellBookApp', []);
+var spellBookApp = angular.module('spellBookApp', ['ui.bootstrap']);
 spellBookApp.config(function($locationProvider) {
   $locationProvider.html5Mode(true);
 });
