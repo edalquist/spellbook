902c496e248a9fb18343328cb3ae81839314b7c9
Eric Dalquist <eric.dalquist@gmail.com>
2014-10-12 00:15:31 -0700

    Start on search filtering

--- src/app/search/search.controller.js
+++ src/app/search/search.controller.js
@@ -13,4 +13,36 @@ angular.module('spellbook')
     bindP.then(function() {
     	console.log($scope.spells);
     });
+
+    $scope.levels = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    $scope.classes = ['Bard', 'Cleric', 'Druid', 'Paladin', 'Ranger', 'Sorcerer', 'Warlock', 'Wizard'];
+    $scope.schools = ['Abjuration', 'Conjuration', 'Divination', 'Enchantment', 'Evocation', 'Illusion', 'Necromancy', 'Transmutation'];
+
+    $scope.search = {};
+
+    $scope.filterSpells = function(spells) {
+    	var filteredSpells = {};
+    	angular.forEach(spells, function(value, key) {
+    		if (!angular.isObject(value)) {
+    			return;
+    		}
+
+    		var include = false;
+    		if (angular.isString($scope.search.text) && $scope.search.text.length > 0) {
+    			var searchRegex = new RegExp($scope.search.text, "i");
+    			include = key.search(searchRegex) >= 0;
+    			include = include || value.description.search(searchRegex) >= 0;
+    			include = include || value.duration.search(searchRegex) >= 0;
+    			include = include || value.school.search(searchRegex) >= 0;
+    			// TODO search the classes
+    		} else {
+    			include = true;
+    		}
+
+			  if (include) {
+			  	filteredSpells[key] = value;
+			  }
+			});
+    	return filteredSpells;
+    };
   }]);
