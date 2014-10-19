62b156e1c8d731e9bf35b6f908b4e0db8b3dd991
Eric Dalquist <eric.dalquist@gmail.com>
2014-10-18 21:23:10 -0700

    Fix navigation

--- src/app/search/search.controller.js
+++ src/app/search/search.controller.js
@@ -28,6 +28,7 @@ angular.module('spellbook')
 		$scope.$on('$firebaseSimpleLogin:logout', function() {
 			$scope.$evalAsync(doUnSync);
 		});
+		doSync();
 
 
     $scope.levels = ['Cantrip', 1, 2, 3, 4, 5, 6, 7, 8, 9];
